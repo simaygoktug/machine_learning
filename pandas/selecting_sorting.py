37a773ed4a12691a5c31ad3a9a2504f57005cc9b
simaygoktug <153096319+simaygoktug@users.noreply.github.com>
2024-02-06 18:22:43 +0300

    Create selecting_sorting.py

---
+++ pandas/selecting_sorting.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+
+def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
+    df=pd.DataFrame(animals)
+    selected_animals_df=df[df["weight"]>100]
+    sorted_animals_df=selected_animals_df.sort_values(by='weight', ascending=False)
+    return sorted_animals_df[["names]]
