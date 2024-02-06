0c1e0346b94fa5d9513271c7be266d5041fba5bb
simaygoktug <153096319+simaygoktug@users.noreply.github.com>
2024-02-06 18:23:12 +0300

    Update selecting_sorting.py

--- pandas/selecting_sorting.py
+++ pandas/selecting_sorting.py
@@ -4,4 +4,4 @@ def findHeavyAnimals(animals: pd.DataFrame) -> pd.DataFrame:
     df=pd.DataFrame(animals)
     selected_animals_df=df[df["weight"]>100]
     sorted_animals_df=selected_animals_df.sort_values(by='weight', ascending=False)
-    return sorted_animals_df[["names]]
+    return sorted_animals_df[["names"]]
