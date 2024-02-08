6fa06f54fea13b98c84e8e9583330d4d629b748e
simaygoktug <153096319+simaygoktug@users.noreply.github.com>
2024-02-08 18:35:12 +0300

    Update customer_detecting.py

--- pandas/customer_detecting.py
+++ pandas/customer_detecting.py
@@ -10,3 +10,12 @@ def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFram
     result_df.columns = ['Customers']  
 
     return result_df
+
+#how: Bu parametre, birleştirme işleminin nasıl gerçekleştirileceğini belirler. Genellikle kullanılan değerler şunlardır:
+#'left': Sol tablodaki (ilk belirtilen tablo) tüm satırları korur ve eşleşen satırlar sağ tablodan (ikinci belirtilen tablo) alınır. Eşleşmeyen sağ tablo satırları için NaN değerler eklenir.
+#'right': Sağ tablodaki tüm satırları korur ve eşleşen satırlar sol tablodan alınır. Eşleşmeyen sol tablo satırları için NaN değerler eklenir.
+#'inner': Sadece her iki tabloda da bulunan satırları korur, diğer satırları atar.
+#'outer': Her iki tablodaki tüm satırları korur, eşleşmeyen değerler için NaN değerler eklenir.
+
+    #left_on: Bu parametre, sol tablodaki birleştirme sütununun adını belirtir.
+#right_on: Bu parametre, sağ tablodaki birleştirme sütununun adını belirtir.
